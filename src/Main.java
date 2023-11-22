8719788ad333589a45cc90ab66a0b081a214c6f4
Spurberino <elia07891@gmail.com>
2023-11-22 14:58:14 +0100

    Start of Tema 1
    Implemented start of file reader (WIP)
    Implemented early grass class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import itumulator.executable.DisplayInformation;
 import itumulator.executable.Program;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+
+import itumulator.world.Location;
+import itumulator.world.World;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("T0.1\\t1-1a.txt"));
+        int size = scanner.nextInt();
+        int delay = 100;
+        int display_size = 800;
+        Program program = new Program(size, display_size, delay);
+        World world = program.getWorld();
+        Location place = new Location(0, 0);
+        world.setTile(place, new Grass());
+        program.show();
       
     }
 }
