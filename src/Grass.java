4c3059de7188c9e5f041acc307ddefdb5d9565ae
Spurberino <elia07891@gmail.com>
2023-11-23 16:14:18 +0100

    Add Rabbit class and implement NonBlocking interface in Grass class

--- src/Grass.java
+++ src/Grass.java
@@ -6,14 +6,20 @@ import itumulator.executable.DisplayInformation;
 import itumulator.executable.DynamicDisplayInformationProvider;
 import itumulator.simulator.Actor;
 import itumulator.world.Location;
+import itumulator.world.NonBlocking;
 import itumulator.world.World;
 
-public class Grass implements Actor, DynamicDisplayInformationProvider {
+public class Grass implements Actor, DynamicDisplayInformationProvider, NonBlocking {
     Random r = new Random();
 
     public void act(World world) {
         if (world.isDay() && r.nextInt(0, 9) == 0) {
-                Set<Location> neighbours = world.getEmptySurroundingTiles();
+                Set<Location> neighbours = world.getSurroundingTiles();
+                for (Location l : neighbours) {
+                    if (world.getTile(l) instanceof Grass) {
+                        neighbours.remove(l);
+                    }
+                }
                 List<Location> list = new ArrayList<>(neighbours);
                 if (list.size() > 0) {
                     Location place = list.get(r.nextInt(list.size()));
